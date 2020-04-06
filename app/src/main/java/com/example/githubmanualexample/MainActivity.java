b212a040d7fe52ce33ee688bcde8393afd9d26f4
SVU_591@ukr.net <SVU_591@ukr.net>
2020-04-06 19:48:24 +0300

    create new project

---
+++ app/src/main/java/com/example/githubmanualexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.githubmanualexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+    }
+}
