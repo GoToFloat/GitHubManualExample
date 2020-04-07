d932ec135e286247f3ad116e539f65671bf12ce0
SVU_591@ukr.net <SVU_591@ukr.net>
2020-04-07 22:39:34 +0300

    added toast delete1

--- app/src/main/java/com/example/githubmanualexample/MainActivity.java
+++ app/src/main/java/com/example/githubmanualexample/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "aaa1_vitaliy", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "aaa1_master_viktor", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa1_master_delete1", Toast.LENGTH_SHORT).show();
     }
 }
