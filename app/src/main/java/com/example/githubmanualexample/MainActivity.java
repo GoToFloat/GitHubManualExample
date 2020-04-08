11d8977c8f0d91deb3831c36db838e8d1c433f5d
SVU_591@ukr.net <SVU_591@ukr.net>
2020-04-08 15:41:05 +0300

    initial com

--- app/src/main/java/com/example/githubmanualexample/MainActivity.java
+++ app/src/main/java/com/example/githubmanualexample/MainActivity.java
@@ -23,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
         //Vitalya-victor
 
+<<<<<<< HEAD
        // new Victor
         //NEWvITALIY
+=======
+
+>>>>>>> parent of dedeb9d... initial commit add comment new viktor
 
     }
 
