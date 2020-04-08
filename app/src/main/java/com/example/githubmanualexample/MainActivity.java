96d86ea6d7c0e74aa82108f3f97fd6346e2320b0
ArtemyukViktor <artemyukviktor@ukr.net>
2020-04-08 13:38:07 +0300

    added toast viktor1

--- app/src/main/java/com/example/githubmanualexample/MainActivity.java
+++ app/src/main/java/com/example/githubmanualexample/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "aaa1_vitaliy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "aaa1_vitaliy_viktor1", Toast.LENGTH_SHORT).show();
     }
 }
